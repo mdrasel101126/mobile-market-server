02fbcb94e854209c8d9d911bf86925f3a10f88f0
mdrasel101126 <mdrasel101126@gmail.com>
2022-11-26 15:22:17 +0600

    delete seller api added

--- index.js
+++ index.js
@@ -177,6 +177,15 @@ async function run() {
       );
       res.send(updtaeUser);
     });
+
+    //delete api
+    //delete seller
+    app.delete("/deleteSeller/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     //server never stopped
   }
